c7ad9c1368d41adde5031c84ff051d23219f3a57
lscraft <lvseqiji@vip.qq.com>
2017-10-15 21:12:14 +0800

    Update selpg.go
    bug fixed
    you can use pipe to send info to this program now

--- selpg.go
+++ selpg.go
@@ -10,36 +10,52 @@ import (
 )
 
 func main() {
-	var s, e, l int    
+	var s, e, l int
 	var d string
 	flag.IntVar(&s, "s", 1, "start page, default 1")
 	flag.IntVar(&e, "e", 1, "end page, deafault 1")
 	flag.IntVar(&l, "l", 72, "how many lines in single page, deafault 72")
 	flag.StringVar(&d, "d", "", "pipe or not, default nil")
 	flag.Parse()
-	fileName := flag.Args()[0]
-	f, err := os.Open(fileName)
-	if err != nil {
-		f.Close()
-		os.Exit(1)
-	}
-
 	begin := (s - 1) * l
 	end := e * l
 	count := 1
-	scanner := bufio.NewScanner(f)
 	cmdinfo := ""
-	for scanner.Scan() {
-		line := scanner.Text()
-		if count > begin && count <= end {
-			if d == "" {                    // without d param, then print to stdout
-				fmt.Println(line)
-			} else {
-				cmdinfo = cmdinfo + line + "\n"
+	//without filename ,read info from standard input
+	if len(flag.Args()) == 0 {
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			line := scanner.Text()
+			if count > begin && count <= end {
+				if d == "" {
+					fmt.Println(line)
+				} else {
+					cmdinfo = cmdinfo + line + "\n"
+				}
+			}
+		}
+		//with filename, read info from file
+	} else {
+		fileName := flag.Args()[0]
+		f, err := os.Open(fileName)
+		if err != nil {
+			f.Close()
+			os.Exit(1)
+		}
+		scanner := bufio.NewScanner(f)
+		for scanner.Scan() {
+			line := scanner.Text()
+			if count > begin && count <= end {
+				if d == "" {
+					fmt.Println(line)
+				} else {
+					cmdinfo = cmdinfo + line + "\n"
+				}
 			}
 		}
 	}
-	if d != "" {                       // with d param,  pipe the info read from file to a command
+	//with -d option ,it will pipe info to command "lp -d***"
+	if d != "" {
 		cmdArg := "-d" + d
 		cmd := exec.Command("lp", cmdArg)
 		cmdin, cmderr := cmd.StdinPipe()
@@ -47,6 +63,9 @@ func main() {
 			cmdin.Close()
 			os.Exit(1)
 		}
+		cmd.Start()
 		io.WriteString(cmdin, cmdinfo)
+		cmdin.Close()
+		cmd.Wait()
 	}
 }
